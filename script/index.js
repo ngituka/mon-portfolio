3397acab5beae476573fd4f0d36fa4cdfdc5528a
Jannie Ngoy Ituka <janniengoyituka@icloud.com>
2025-06-04 22:12:01 +0200

    began to implement the scrolling animation, changed the font, translated to french only
    début d'implémentation de l'animation de défilement, changement de police, et traduction en français uniquement

--- script/index.js
+++ script/index.js
@@ -1,67 +1,79 @@
-/*
-function changeLocation()
-{
-    document.getElementsByID("contactsLocation").innerHTML = "in the footer";
+function changeLocation() {
+    const contact = document.getElementById("contactsLocation");
+    if (contact) {
+        contact.innerHTML = "in the footer";
+    }
 }
 
 var estimatedPhoneWidth = 900;
 
-if (screen.width <= estimatedPhoneWidth || window.innerHeight <= estimatedPhoneWidth)
-{
+if (screen.width <= estimatedPhoneWidth || window.innerHeight <= estimatedPhoneWidth) {
     changeLocation();
 }
-*/
 
 var skillButton = document.getElementById("skill-revealer");
+var body = document.getElementsByTagName("body")[0];
 
-document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById("skill-revealer").addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  /*animation des competences*/  
+  document.getElementById("skill-revealer").addEventListener("click", () => {
         animateProgressBar("html", 50);
         animateProgressBar("css", 50);
         animateProgressBar("sql", 20);
         animateProgressBar("cpp", 60);
-        document.getElementById("skill-revealer").disabled = "true";
-    
+        document.getElementById("skill-revealer").disabled = true;
     });
 
-    var body = document.getElementsByTagName("body")[0];
-    var beginningPosition = scrollY;
-    body.addEventListener("scroll", () => {
-        scrollDown(beginningPosition)
-    })
+  /*apparition de la barre de navigation*/
+  let previousPosition = window.scrollY;
+  window.addEventListener('scroll', (event) => {
+    console.log('Scroll event fired!');
+    displayNavbar(previousPosition)
+  });
+
+
 });
 
-function animateProgressBar(progressElementID, targetValue){
+// Animate progress bars
+function animateProgressBar(progressElementID, targetValue) {
     const progress = document.getElementById(progressElementID);
     var value = 0;
     const TARGET_VALUE = targetValue;
-  
+
     const interval = setInterval(() => {
-      if (value >= TARGET_VALUE) {
-        clearInterval(interval); // stop when done
-      } else {
-        value+=0.5;
-        progress.value = value;
-      }
-    }, 5); // speed: smaller = faster
+        if (value >= TARGET_VALUE) {
+            clearInterval(interval);
+        } else {
+            value += 0.5;
+            progress.value = value;
+        }
+    }, 5);
+
+    console.log("progress bars have been animated");
 }
 
-function switchPageTheme()
-{
-  var Page = document.getElementsByTagName("body")[0]
-  Page.style.backgroundColor = "red";
-  console.log("color chanfged to red !");
+// Change background color
+function switchPageTheme() {
+    var Page = document.getElementsByTagName("body")[0];
+    Page.style.backgroundColor = "red";
+    console.log("color chanfged to red !");
 }
 
-function scrollDown(beginningPosition)
+function displayNavbar(previousPosition)
 {
-  var currentPosition = scrollY;
-  if (beginningPosition > currentPosition)
-    {
-      var navbar = document.getElementsByTagName("nav")[0];
-      navbar.style.position = "fixed";
-    }
-  
+  let currentPosition = window.scrollY;
+  if (previousPosition > currentPosition)
+  {
+    let navbar = document.getElementsByTagName("nav")[0];
+    navbar.style.position = "fixed";
+  }
+  else
+  {
+    let navbar = document.getElementsByTagName("nav")[0];
+    navbar.style.position = "sticky";
+  }
+
+  previousPosition = currentPosition;
 }
 
 
